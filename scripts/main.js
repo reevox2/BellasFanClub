5c9c6f836121dbc94a96a83ffcea1788f226580c
wonderbeaver <morancy.joel@gmail.com>
2018-04-14 03:08:26 -0500

    Massive Revamp. Added Parallex. Added Videos. Added Grid. Added JS welcome message.

--- scripts/main.js
+++ scripts/main.js
@@ -9,18 +9,55 @@ if (1 === 1) {
 }
 }
 
-var myImg = document.querySelector('img');
+var myImg = document.querySelector('#bella');
 var myImgDescription = document.getElementById('action');
+var media1Description = document.getElementById('action1');
+var media2Description = document.getElementById('action2');
+var media1 = document.querySelector('.dance1');
+var media2 = document.querySelector('.dance2');
 
-
+/* Set Username*/
+function setUsrname(usrname){
+	var name = prompt("Please enter your name.")
+	return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase(); 
+}
 
 myImg.onclick = function () {
 	var mySrc = myImg.getAttribute('src');
-	if(mySrc === 'images/bella-pose.jpg') {
-		myImg.setAttribute('src','images/bella-pose2.jpg');
+	if(mySrc === 'media/bella-pose.jpg') {
+		myImg.setAttribute('src','media/bella-pose2.jpg');
+		media1.setAttribute('src', 'media/sing1.mp4');
+		media2.setAttribute('src', 'media/sing2.mp4');
 		myImgDescription.textContent = "SINGS";
+		media1Description.textContent = "KOREAN BEONCE?";
+		media2Description.textContent = "NEEDS TO RELEASE HER NEXT ALBUM";
 	} else {
-		myImg.setAttribute('src', 'images/bella-pose.jpg');
+		myImg.setAttribute('src', 'media/bella-pose.jpg');
+		media1.setAttribute('src', 'media/dance1.mp4');
+		media2.setAttribute('src', 'media/dance2.mp4');
 		myImgDescription.textContent = "DANCES";
 	}
 }
+/*Welcome Message: Get Time of Day and Name and greet user */
+function currentTime(){
+		usrname = setUsrname();
+		timeNow = new Date().toLocaleTimeString();
+		let amPM = timeNow.charAt(timeNow.length - 2) + timeNow.charAt(timeNow.length - 1);
+		let hour = new Date().getHours()
+		if (amPM === "AM") {
+			if (hour > 5) {
+				document.querySelector('.greeting').textContent = "Good Morning, " + usrname
+			} else {
+				document.querySelector('.greeting').textContent = "I hope you sleep soon, " + usrname
+			}
+		} else if (hour = 12 || hour < 5) {
+			document.querySelector('.greeting').textContent = "Good Afternoon, " + usrname
+		} else if (hour > 5 && hour < 8 ) {
+			document.querySelector('.greeting').textContent = "Good Evening, " + usrname
+		} else if (hour > 9) {
+			document.querySelector('.greeting').textContent = "Having a nice night " + usrname + "?"
+		} else {
+			document.querySelector('.greeting').textContent = "Having a nice night " + usrname + "?"
+		}
+	}
+currentTime();
